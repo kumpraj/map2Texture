08f49de62103493bccac2614cbd31bab10a16111
Prajwal Kumar <kumpraj@gmail.com>
2023-03-17 00:55:39 +0530

    first commit

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+//  creates an Express application.
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//  returns middleware that parses json
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.listen(() => {
+    console.log(`Server is running on port ${port}`);
+});
