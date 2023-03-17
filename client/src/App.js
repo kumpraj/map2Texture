db6ceecc8c36a10eaa5dcf1a64fb1aedb449ef55
Prajwal Kumar <kumpraj@gmail.com>
2023-03-18 02:29:36 +0530

    created frontend client setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Map from './components/Map'
+
+function App() {
+  return (
+    <div>
+      <h1>Map2Texture Mern Stack App</h1>
+      <Map/>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Map.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Map = () => {
+  return (
+    <div>Map Component</div>
+  )
+}
+
+export default Map
